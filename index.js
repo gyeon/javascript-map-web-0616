ddb4142ae460b9c930d76eae65933ed8162136af
gyeon <gyeon19@gmail.com>
2016-07-25 23:08:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const app = "I don't do much."
+const transformedAutobotsWithMap = autobots.map(function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+
+console.log(transformedAutobotsWithMap);
